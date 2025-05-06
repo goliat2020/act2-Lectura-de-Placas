22f9e07fe603f52832eac965b8db9e07cdbefa3f
goliat2020 <alejandroguza44@hotmail.com>
2025-05-06 07:48:50 -0700

    Agregar comentarios al codigo

--- deteccionPlacas.py
+++ deteccionPlacas.py
@@ -1,7 +1,9 @@
 import cv2
+#Instalar easyocr antes de ejecutar el codigo
 import easyocr
 from google.colab.patches import cv2_imshow
 
+#Asignar las rutas de las imagenes
 imagen1= "/content/placa_q.jpg"
 imagen2= "/content/placa_4.jpg"
 
@@ -14,8 +16,10 @@ def detectar_letras_centrales(ruta_imagen):
     # Conversión a escala de grises
     gris = cv2.cvtColor(imagen_original, cv2.COLOR_BGR2GRAY)
 
+    #Aplicar el gaussian blur
     blur = cv2.GaussianBlur(gris, (5,5), 0)
 
+    #Aplicar el threshold
     th = cv2.adaptiveThreshold(
     blur, 255,
     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -23,20 +27,28 @@ def detectar_letras_centrales(ruta_imagen):
     blockSize=15,
     C=4
     )
+    
+    #Definir el kernel
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    
+    #Cambiar la morfologia de la imagen
     closing = cv2.morphologyEx(th, cv2.MORPH_CLOSE, kernel, iterations=5)
 
 
     return closing
 
-
+#Llamar a la funcion por cada imagen
 placa1 = detectar_letras_centrales(imagen1)
 placa2 = detectar_letras_centrales(imagen2)
 
+#Mostrar las imagenes 
 cv2_imshow(placa1)
 cv2_imshow(placa2)
 
+#Definir el lector
 reader = easyocr.Reader(['en','es'], gpu=False)  # ajustar idiomas según necesidad
+
+#Procesar los resultados
 results = reader.readtext(placa1,
                           detail=1,
                           paragraph=False,
@@ -49,6 +61,7 @@ results2 = reader.readtext(placa2,
                           contrast_ths=0.05,
                           adjust_contrast=0.7)
 
+#Imprimir los resultados
 print("EasyOCR encontró:")
 for _, text, prob in results:
     print(f"  - '{text}'  (confianza: {prob:.2f})")
